9d5eb1cc3047673fab63de94875b4439896aef8c
Don Maestro <33356862+nikoi-addo@users.noreply.github.com>
2021-08-19 10:28:10 +0000

    front end changes

--- ALCform.php
+++ ALCform.php
@@ -24,47 +24,7 @@
 </head>
 <body class="vertical-layout">
     <!-- Start Infobar Setting Sidebar -->
-    <div id="infobar-settings-sidebar" class="infobar-settings-sidebar">
-        <div class="infobar-settings-sidebar-head d-flex w-100 justify-content-between">
-            <h4>Settings</h4><a href="javascript:void(0)" id="infobar-settings-close" class="infobar-settings-close"><img src="assets/images/svg-icon/close.svg" class="img-fluid menu-hamburger-close" alt="close"></a>
-        </div>
-        <div class="infobar-settings-sidebar-body">
-            <div class="custom-mode-setting">
-                <div class="row align-items-center pb-3">
-                    <div class="col-8"><h6 class="mb-0">Payment Reminders</h6></div>
-                    <div class="col-4 text-right"><input type="checkbox" class="js-switch-setting-first" checked /></div>
-                </div>
-                <div class="row align-items-center pb-3">
-                    <div class="col-8"><h6 class="mb-0">Stock Updates</h6></div>
-                    <div class="col-4 text-right"><input type="checkbox" class="js-switch-setting-second" checked /></div>
-                </div>
-                <div class="row align-items-center pb-3">
-                    <div class="col-8"><h6 class="mb-0">Open for New Products</h6></div>
-                    <div class="col-4 text-right"><input type="checkbox" class="js-switch-setting-third" /></div>
-                </div>
-                <div class="row align-items-center pb-3">
-                    <div class="col-8"><h6 class="mb-0">Enable SMS</h6></div>
-                    <div class="col-4 text-right"><input type="checkbox" class="js-switch-setting-fourth" checked /></div>
-                </div>
-                <div class="row align-items-center pb-3">
-                    <div class="col-8"><h6 class="mb-0">Newsletter Subscription</h6></div>
-                    <div class="col-4 text-right"><input type="checkbox" class="js-switch-setting-fifth" checked /></div>
-                </div>
-                <div class="row align-items-center pb-3">
-                    <div class="col-8"><h6 class="mb-0">Show Map</h6></div>
-                    <div class="col-4 text-right"><input type="checkbox" class="js-switch-setting-sixth" /></div>
-                </div>
-                <div class="row align-items-center pb-3">
-                    <div class="col-8"><h6 class="mb-0">e-Statement</h6></div>
-                    <div class="col-4 text-right"><input type="checkbox" class="js-switch-setting-seventh" checked /></div>
-                </div>
-                <div class="row align-items-center">
-                    <div class="col-8"><h6 class="mb-0">Monthly Report</h6></div>
-                    <div class="col-4 text-right"><input type="checkbox" class="js-switch-setting-eightth" checked /></div>
-                </div>
-            </div>
-        </div>
-    </div>
+    
     <div class="infobar-settings-sidebar-overlay"></div>
     <!-- End Infobar Setting Sidebar -->
     <!-- Start Containerbar -->
@@ -336,124 +296,8 @@
                 <div class="row align-items-center">
                     <!-- Start col -->
                     <div class="col-md-12 align-self-center">
-                        <div class="togglebar">
-                            <ul class="list-inline mb-0">
-                                <li class="list-inline-item">
-                                    <div class="menubar">
-                                        <a class="menu-hamburger" href="javascript:void();">
-                                           <img src="assets/images/svg-icon/collapse.svg" class="img-fluid menu-hamburger-collapse" alt="collapse">
-                                           <img src="assets/images/svg-icon/close.svg" class="img-fluid menu-hamburger-close" alt="close">
-                                         </a>
-                                     </div>
-                                </li>
-                                <li class="list-inline-item">
-                                    <div class="searchbar">
-                                        <form>
-                                            <div class="input-group">
-                                              <input type="search" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="button-addon2">
-                                              <div class="input-group-append">
-                                                <button class="btn" type="submit" id="button-addon2"><img src="assets/images/svg-icon/search.svg" class="img-fluid" alt="search"></button>
-                                              </div>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="infobar">
-                            <ul class="list-inline mb-0">
-                                <li class="list-inline-item">
-                                    <div class="settingbar">
-                                        <a href="javascript:void(0)" id="infobar-settings-open" class="infobar-icon">
-                                            <img src="assets/images/svg-icon/settings.svg" class="img-fluid" alt="settings">
-                                        </a>
-                                    </div>
-                                </li>
-                                <li class="list-inline-item">
-                                    <div class="notifybar">
-                                        <div class="dropdown">
-                                            <a class="dropdown-toggle infobar-icon" href="#" role="button" id="notoficationlink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="assets/images/svg-icon/notifications.svg" class="img-fluid" alt="notifications">
-                                            <span class="live-icon"></span></a>
-                                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="notoficationlink">
-                                                <div class="notification-dropdown-title">
-                                                    <h4>Notifications</h4>                            
-                                                </div>
-                                                <ul class="list-unstyled">                                                    
-                                                    <li class="media dropdown-item">
-                                                        <span class="action-icon badge badge-primary-inverse"><i class="feather icon-dollar-sign"></i></span>
-                                                        <div class="media-body">
-                                                            <h5 class="action-title">$135 received</h5>
-                                                            <p><span class="timing">Today, 10:45 AM</span></p>                            
-                                                        </div>
-                                                    </li>
-                                                    <li class="media dropdown-item">
-                                                        <span class="action-icon badge badge-success-inverse"><i class="feather icon-file"></i></span>
-                                                        <div class="media-body">
-                                                            <h5 class="action-title">Project X prototype approved</h5>
-                                                            <p><span class="timing">Yesterday, 01:40 PM</span></p>                            
-                                                        </div>
-                                                    </li>
-                                                    <li class="media dropdown-item">
-                                                        <span class="action-icon badge badge-danger-inverse"><i class="feather icon-eye"></i></span>
-                                                        <div class="media-body">
-                                                            <h5 class="action-title">John requested to view wireframe</h5>
-                                                            <p><span class="timing">3 Sep 2019, 05:22 PM</span></p>                            
-                                                        </div>
-                                                    </li>
-                                                    <li class="media dropdown-item">
-                                                        <span class="action-icon badge badge-warning-inverse"><i class="feather icon-package"></i></span>
-                                                        <div class="media-body">
-                                                            <h5 class="action-title">Sports shoes are out of stock</h5>
-                                                            <p><span class="timing">15 Sep 2019, 02:55 PM</span></p>
-                                                        </div>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </li>                                
-                                <li class="list-inline-item">
-                                    <div class="languagebar">
-                                        <div class="dropdown">
-                                            <a class="dropdown-toggle" href="#" role="button" id="languagelink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="flag flag-icon-us flag-icon-squared"></i></a>
-                                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="languagelink">
-                                                <a class="dropdown-item" href="#"><i class="flag flag-icon-us flag-icon-squared"></i>English</a>
-                                                <a class="dropdown-item" href="#"><i class="flag flag-icon-de flag-icon-squared"></i>German</a>
-                                                <a class="dropdown-item" href="#"><i class="flag flag-icon-bl flag-icon-squared"></i>France</a>
-                                                <a class="dropdown-item" href="#"><i class="flag flag-icon-ru flag-icon-squared"></i>Russian</a>                                                
-                                            </div>
-                                        </div>
-                                    </div>                                   
-                                </li>
-                                <li class="list-inline-item">
-                                    <div class="profilebar">
-                                        <div class="dropdown">
-                                          <a class="dropdown-toggle" href="#" role="button" id="profilelink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="assets/images/users/profile.svg" class="img-fluid" alt="profile"><span class="feather icon-chevron-down live-icon"></span></a>
-                                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="profilelink">
-                                                <div class="dropdown-item">
-                                                    <div class="profilename">
-                                                      <h5>John Doe</h5>
-                                                    </div>
-                                                </div>
-                                                <div class="userbox">
-                                                    <ul class="list-unstyled mb-0">
-                                                        <li class="media dropdown-item">
-                                                            <a href="#" class="profile-icon"><img src="assets/images/svg-icon/user.svg" class="img-fluid" alt="user">My Profile</a>
-                                                        </li>
-                                                        <li class="media dropdown-item">
-                                                            <a href="#" class="profile-icon"><img src="assets/images/svg-icon/email.svg" class="img-fluid" alt="email">Email</a>
-                                                        </li>                                                        
-                                                        <li class="media dropdown-item">
-                                                            <a href="#" class="profile-icon"><img src="assets/images/svg-icon/logout.svg" class="img-fluid" alt="logout">Logout</a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>                                   
-                                </li>
-                            </ul>
-                        </div>
+                       
+                       
                     </div>
                     <!-- End col -->
                 </div> 
@@ -512,11 +356,11 @@
                                 <div class="form-row">
                                     <div class="form-group col-md-6">
                                         <label for="firstname">First Name</label>
-                                        <input type="text" class="form-control" name="firstname" id="firstname" placeholder="First Name">
+                                        <input type="text" class="form-control" name="firstname" id="firstname" placeholder="eg. John" required>
                                     </div>
                                     <div class="form-group col-md-6">
                                         <label for="lastname">Last Name</label>
-                                        <input type="text" class="form-control" name="lastname" id="lastname" placeholder="Last Name">
+                                        <input type="text" class="form-control" name="lastname" id="lastname" placeholder="eg. Akua Asamoah" required>
                                     </div>
                                     <div class="form-group col-md-6">
                                         <label for="dob">Date Of Birth</label>
@@ -537,6 +381,10 @@
                                         </div>
                                         
                                     </div>
+                                     <div class="form-group col-md-6">
+                                        <label for="firstname">Assembly of Fellowship</label>
+                                        <input type="text" class="form-control" name="church" id="church" placeholder="eg. The Temple of Charis" required>
+                                    </div>
                                 </div>
                             </div>
                             <div class="card-header">
@@ -551,11 +399,11 @@
                                     </div>
                                     <div class="form-group col-md-6">
                                         <label for="inputTel">Phone Number</label>
-                                    <input type="tel" class="form-control" name="telephone" id="inputTel" placeholder="0200000000">
+                                    <input type="tel" class="form-control" name="telephone" id="inputTel" placeholder="0200000000" required>
                                     </div>
                                     <div class="form-group col-md-6">
                                         <label for="inputText">Location</label>
-                                        <input type="text" class="form-control" name="location" id="inputText" placeholder="Location">
+                                        <input type="text" class="form-control" name="location" id="inputText" placeholder="eg. Latebiokoshie" required>
                                     </div>
                                 </div>
                             </div>
@@ -565,7 +413,7 @@
                             <div class="card-body">
                             
                                 <div class="form-row">
-                                    <div class="form-group col-md-6">
+                                    <div class="form-group col-md-6" required>
                                         <label>How did you hear about the program?</label>
                                         <div class="custom-control custom-radio">
                                             <input type="radio" id="radio1" name="customradio" class="custom-control-input" value="Social Media">
@@ -592,7 +440,7 @@
                                         <label for="inputPassword4">What are your expectations for ALC 2021?</label>
                                         <div class="input-group-prepend">
                                             <span class="input-group-text">Expectations :</span>
-                                            <textarea class="form-control" aria-label="With textarea" name="expectations"></textarea>
+                                            <textarea class="form-control" aria-label="With textarea" name="expectations" required></textarea>
                                         </div>
 
                                         
@@ -625,7 +473,7 @@
                                     
                                     </div>
                                   
-                                    <div class="form-group col-md-6">
+                                    <div class="form-group col-md-6" required>
                                         <label for="#">On which days will you be attending ALC 2021?</label>
                                         <div class="custom-control custom-switch">
 
@@ -702,15 +550,7 @@
     <!-- Core js -->
     <script src="assets/js/core.js"></script>
     
-    <script>
-        $(document).ready(function () {
-            $("#radio1").click(function () {
-
-                $('div.smedia').hide();
-                $('#sm').show;
-            });
-        });
-    </script>
+    
     <!-- End js -->  
 </body>
 
